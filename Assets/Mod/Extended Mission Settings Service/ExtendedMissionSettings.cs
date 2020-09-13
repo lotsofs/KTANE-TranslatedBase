3db333071c7da0f890d7ab633b52ec6a13de9bef
lotsofs <oeneboy@hotmail.com>
2020-09-13 02:08:23 +0200

    Figured out how to put the extended mission settings in the mission file itself and make sure the mission still runs :)

--- Assets/Mod/Extended Mission Settings Service/ExtendedMissionSettings.cs
+++ Assets/Mod/Extended Mission Settings Service/ExtendedMissionSettings.cs
@@ -1,41 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using KModkit;
 using System;
 using System.Reflection;
+using Newtonsoft.Json.Linq;
 
 public class ExtendedMissionSettings : MonoBehaviour {
 
-	public static Dictionary<string, Dictionary<string, string>> RegisteredMissions = new Dictionary<string, Dictionary<string, string>>();
+	public static Dictionary<string, string> ExtendedMissions = new Dictionary<string, string>();
 	
-	[SerializeField] KMBombInfo _bombInfo;
-	[HideInInspector] public static bool Ready = false;
-	[HideInInspector] public static bool BombStarted = false;
+	[SerializeField] KMGameInfo _gameInfo;
 
-	[HideInInspector] public Dictionary<string, string> CurrentMissionSettings = null;
+	[HideInInspector] public bool _finished = false;
+	[HideInInspector] public string CurrentMissionSettings = null;
+
+	bool _disabled = false;
 
 	void Start() {
-		Ready = true;
+		_gameInfo.OnStateChange += OnGameStateChange;
+		Debug.Log("[Extended Mission Settings] Hello! :)");
 	}
 
-	void Update() {
-		// todo make this a courotine
-		if (!BombStarted && _bombInfo.IsBombPresent()) {
-			string missionName = GetMissionName();
-			if (RegisteredMissions.ContainsKey(missionName)) {
-				Debug.LogFormat("[Extended Mission Settings] Found {1} settings for mission {0}. ", missionName, RegisteredMissions[missionName].Count);
-				CurrentMissionSettings = RegisteredMissions[missionName];
-			}
-			else {
-				Debug.LogFormat("[Extended Mission Settings] No settings found for mission {0}", missionName);
-				CurrentMissionSettings = new Dictionary<string, string>();
-			}
-			BombStarted = true;
+	void OnDisable() {
+		_disabled = true;
+	}
+
+	void OnEnable() {
+		if (_disabled) {
+			Debug.Log("[Extended Mission Settings] Hello again! :)");
+			_disabled = false;
+			FindMissionSettings();
+			_finished = true;
 		}
-		else if (!_bombInfo.IsBombPresent() && BombStarted) {
-			BombStarted = false;
-			CurrentMissionSettings = null;
+	}
+
+	void OnGameStateChange(KMGameInfo.State state) {
+		switch (state) {
+			case KMGameInfo.State.Setup:
+				CurrentMissionSettings = null;
+				if (!_finished) {
+					FindMissionSettings();
+					_finished = true;
+				}
+				break;
+			case KMGameInfo.State.Gameplay:
+				GetMissionSettings(GetMissionName());
+				break;
 		}
 	}
 
@@ -45,12 +55,116 @@ public class ExtendedMissionSettings : MonoBehaviour {
 			Type type = gameplayState.GetType();
 			FieldInfo fieldMission = type.GetField("MissionToLoad", BindingFlags.Public | BindingFlags.Static);
 			string currentMission = fieldMission.GetValue(gameplayState).ToString();
+			Debug.LogFormat("[Extended Mission Settings] Detected mission: {0}.", currentMission);
 			return currentMission;
 		}
 		catch (Exception e) {
-			Debug.Log("[Extended Mission Settings] Ran into an issue when fetching the mission name.");
-			Debug.Log(e.Message);
+			Debug.LogFormat("[Extended Mission Settings] Ran into an issue when fetching the current mission name: {0}", e.Message);
 			return "unknown";
 		}
+	}
+
+	void GetMissionSettings(string missionName) {
+		if (ExtendedMissions.ContainsKey(missionName)) {
+			Debug.LogFormat("[Extended Mission Settings] Found settings for mission {0}: {1}", missionName, ExtendedMissions[missionName]);
+			CurrentMissionSettings = ExtendedMissions[missionName];
+		}
+		else {
+			Debug.LogFormat("[Extended Mission Settings] No settings found for mission {0}", missionName);
+			CurrentMissionSettings = "";
+		}
+	}
+
+	/// <summary>
+	/// Scans through ALL the modded missions to find any that contain our extended mission settings.
+	/// TODO: Should probably only scan missions that are opened in the binder. This looks like a long process when many missions are present.
+	/// </summary>
+	/// <returns></returns>
+	void FindMissionSettings() {
+		DateTime start = DateTime.Now;
+		Debug.Log("[Extended Mission Settings] Searching missions for extended settings.");
+
+		// Fetch a list of all scriptable objects with type 'ModMission'. TODO: This can probably be done more efficiently without fetching ALL scriptobs first.
+		var missions = new List<ScriptableObject>();
+		foreach (var obj in GameObject.FindObjectsOfType<ScriptableObject>()) {
+			if (obj.GetType().ToString() == "ModMission") {
+				missions.Add(obj);
+			}
+		}
+		Debug.LogFormat("[Extended Mission Settings] Checking {0} missions...", missions.Count);
+
+		Type typeGS = missions[0].GetType();    // Todo: "ModMission", I already know that, but I don't know how to tell the script this upfront.
+		FieldInfo fieldGeneratorSetting = typeGS.GetField("GeneratorSetting", BindingFlags.Public | BindingFlags.Instance);
+
+		// get the type for generator settings here so we dont do it every time
+		Type typeCP = fieldGeneratorSetting.GetValue(missions[0]).GetType();
+
+		foreach (ScriptableObject mission in missions) {
+			// get generator settings for this mission
+			var generatorSettings = fieldGeneratorSetting.GetValue(mission);
+
+			// get a list of component pools for this mission
+			FieldInfo fieldComponentPool = typeCP.GetField("ComponentPools", BindingFlags.Public | BindingFlags.Instance);
+			IList componentPools = fieldComponentPool.GetValue(generatorSettings) as IList;
+
+			// component pools contain mod types that determine which mods are in that component pool
+			Type typeMT = componentPools[0].GetType();
+			FieldInfo fieldModTypes = typeMT.GetField("ModTypes", BindingFlags.Public | BindingFlags.Instance);
+
+			List<object> componentPoolsToClear = new List<object>();
+			foreach (var cp in componentPools) {
+				IList<string> modTypes = fieldModTypes.GetValue(cp) as IList<string>;
+				for (int i = modTypes.Count - 1; i >= 0; i--) {
+					if (modTypes[i].StartsWith("Extended Settings")) {
+						if (AddExtendedSettings(mission.name, modTypes[i]))
+							componentPoolsToClear.Add(cp);
+					}
+				}
+			}
+			// when we found our settings, remove their containing component pool so that the mission binder won't complain about 'missing mods'
+			foreach (var cp in componentPoolsToClear) {
+				componentPools.Remove(cp);
+			}
+		}
+		TimeSpan duration = DateTime.Now - start;
+		Debug.LogFormat("[Extended Mission Settings] Finished processing missions. Took {0} seconds.", duration.TotalSeconds);
+	}
+
+	/// <summary>
+	/// Adds the settings to the mission.
+	/// </summary>
+	/// <param name="mission"></param>
+	/// <param name="modType"></param>
+	/// <returns>Whether it was succesful</returns>
+	bool AddExtendedSettings(string mission, string modType) {
+		string settings;
+		try {
+			settings = modType.Substring(modType.IndexOf('{')).Trim();
+		}
+		catch (Exception e) {
+			Debug.LogFormat("[Extended Mission Settings] Encountered missing Json in mission {0}: {1}", mission, modType);
+			return false;
+		}
+		
+		JObject o1;
+		try {
+			o1 = JObject.Parse(settings);
+		}
+		catch (Exception e) {
+			Debug.LogFormat("[Extended Mission Settings] Encountered invalid Json in mission {0}: {1}", mission, settings);
+			return false;
+		}
+
+		if (ExtendedMissions.ContainsKey(mission)) {
+			Debug.LogFormat("[Extended Mission Settings] Adding settings to settings pool for mission {0}: {1}", mission, settings);
+			JObject o2 = JObject.Parse(ExtendedMissions[mission]);
+			o2.Merge(o1, new JsonMergeSettings { MergeArrayHandling = MergeArrayHandling.Union });
+			ExtendedMissions[mission] = o2.ToString();
+		}
+		else {
+			Debug.LogFormat("[Extended Mission Settings] Found settings for mission {0}: {1}", mission, settings);
+			ExtendedMissions.Add(mission, settings);
+		}
+		return true;
 	}
 }
