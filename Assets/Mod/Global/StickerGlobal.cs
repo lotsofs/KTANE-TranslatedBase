51099fc10d44775104082080c2a06485fe4ac389
lotsofs <oeneboy@hotmail.com>
2020-10-04 17:51:50 +0200

    Overdesigned sticker

--- Assets/Mod/Global/StickerGlobal.cs
+++ Assets/Mod/Global/StickerGlobal.cs
@@ -6,34 +6,74 @@ using UnityEngine;
 
 public class StickerGlobal : MonoBehaviour {
 
-	[SerializeField] TextMesh _bigLabel;
-	[SerializeField] TextMesh _blackBar;
+	[SerializeField] TextMesh _labelLang;
+	[SerializeField] TextMesh _labelLang3;
+	[SerializeField] TextMesh _labelScript;
+	[SerializeField] TextMesh _labelRegion;
+	[SerializeField] TextMesh _blackBarTop;
+	[SerializeField] TextMesh _blackBarBottom;
 
-	const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	const string CHARS_ALPHANUMERIC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	const string CHARS_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	const string CHARS_NOTHEXADECIMAL = "GHIJKLMNOPQRSTUVWXYZ";
 
-	/// <summary>
-	/// Adds some aesthetics to the sticker
-	/// </summary>
-	public void GenerateText(string iso, int langVersion, string periodicTableSymbol) {
-		_bigLabel.text = iso.ToUpperInvariant();
+	public void GenerateText(string language, int langVersion, string periodicTableSymbol) {
+		_labelLang.text = language;
+		if (language.Length == 3) _labelLang.fontSize = 222;
+		_labelScript.text = string.Empty;
+		_labelRegion.text = string.Empty;
 
+		TopBlackBar(language, string.Empty, periodicTableSymbol);
+		BottomBlackBar(langVersion, string.Empty);
+	}
+
+	void GenerateText(string language, string region, string script, int langVersion, string periodicTableSymbol) {
+		if (language.Length == 3) {
+			_labelLang.gameObject.SetActive(false);
+			_labelLang3.gameObject.SetActive(true);
+			_labelLang = _labelLang3;
+		}
+		
+		_labelLang.text = language;
+		_labelScript.text = script;
+		_labelRegion.text = region;
+
+		TopBlackBar(language, region, periodicTableSymbol);
+		BottomBlackBar(langVersion, script);
+	}
+
+	public void TopBlackBar(string language, string region, string periodicTableSymbol) {
 		string hexPTC = "";
 		foreach (char c in periodicTableSymbol) {
 			hexPTC += Convert.ToInt32(c).ToString("X");
 		}
 
 		string randomChars = "";
-		for (int i = 0; i < 5; i++) {
-			randomChars += chars[UnityEngine.Random.Range(0, chars.Length)];
+		for (int i = 0; i < 10 - language.Length; i++) {
+			randomChars += CHARS_ALPHANUMERIC[UnityEngine.Random.Range(0, CHARS_ALPHANUMERIC.Length)];
 		}
 
-		// {1} is a random 5-digit alphanumeric code. Local version will only have 4 digits, to tell them apart.
-		_blackBar.text = string.Format("TLM{0}.{1}.{2}{3}{4}", 
-			hexPTC, 
-			randomChars, 
-			iso.ToUpperInvariant(), 
-			UnityEngine.Random.Range(0, 1000).ToString("000"),
-			langVersion.ToString()
+		_blackBarTop.text = string.Format("{0}.{1}.{2}{3}{4}",
+			hexPTC,
+			randomChars,
+			language.ToUpperInvariant(),
+			region.ToUpperInvariant(),
+			UnityEngine.Random.Range(0, 1000).ToString("000")
+		);
+	}
+
+	public void BottomBlackBar(int langVersion, string script) {
+		string randomChars = "";
+		for (int i = script.Length; i < 5; i++) {
+			randomChars += CHARS_ALPHABET[UnityEngine.Random.Range(0, CHARS_ALPHABET.Length)];
+		}
+
+		_blackBarBottom.text = string.Format("TLM:{0}{1}371{2}{3}{4}",
+			randomChars,
+			script.ToUpperInvariant(),
+			UnityEngine.Random.Range(0, 0x1000).ToString("X3"),
+			CHARS_NOTHEXADECIMAL[UnityEngine.Random.Range(0, CHARS_NOTHEXADECIMAL.Length)],
+			langVersion
 		);
 	}
 }
